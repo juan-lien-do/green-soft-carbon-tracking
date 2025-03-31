a2d068c20e69518b3965d928becf2e59e03033de
Juan Liendo <findmejuan@hotmail.com>
2025-03-31 19:27:05 -0300

    agregar datos xd

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ def main():
     )
 
     # MODIFICA ESTA LINEA PARA CAMBIAR LA CANTIDAD DE TIEMPO DURANTE EL MUESTREO
-    minutos = 1
+    minutos = 10
 
     # Fuerza el factor de emisión (para versiones que no lo soportan en el constructor)
     os.environ["CO2_SIGNAL_API_KEY"] = "dummy"  # Evita errores de API
@@ -27,12 +27,38 @@ def main():
             time.sleep(1)  # Simula procesamiento
 
     finally:
+        # arbol = 0,34 ton co2
+
         # Detiene el tracker y muestra resultados
         emissions = tracker.stop()
+
+        # cantidad co2 en tanto tiempo (minutos)
+        cantidad = emissions
+        #1440 minutos en un dia
+        equivalencia_dia_anio = 365
+        equivalencia_doshoras = 120 / minutos
+
+        energia_sesion = tracker._total_energy.kWh
+        emision_sesion = emissions
+
+        energia_doshoras = energia_sesion * equivalencia_doshoras
+        emision_doshoras = emision_sesion * equivalencia_doshoras
+
+        energia_anio = energia_doshoras * equivalencia_dia_anio
+        emision_anio = emision_doshoras * equivalencia_dia_anio
+
         print("\n--- Resultados ---")
-        print(f"Consumo de energía: {tracker._total_energy.kWh:.6f} kWh")
-        print(f"Emisiones de CO₂eq: {emissions:.6f} kg")
-        print(f"Factor de emisión usado: 0.26 kg CO₂eq/kWh (fijado manualmente)")
+        print(f"Consumo de energía de la sesión: {energia_sesion:.6f} kWh")
+        print(f"Emisiones de CO₂eq de la sesión: {emision_sesion:.6f} kg \n")
+
+        print(f"Consumo de energía de dos horas: {energia_doshoras:.6f} kWh")
+        print(f"Emisiones de CO₂eq de dos horas: {emision_doshoras:.6f} kg \n")
+
+        print("ESTIMADO")
+        print(f"Consumo de energía de dos horas por día al año: {energia_anio:.6f} kWh")
+        print(f"Emisiones de CO₂eq de dos horas por día al año: {emision_anio:.6f} kg \n")
+
+        print(f"Factor de emisión usado: 0.26 kg CO₂eq/kWh")
 
 if __name__ == "__main__":
     main()
